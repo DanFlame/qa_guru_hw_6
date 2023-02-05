00e5ff9677fc0e0e0fa4b6c6ea77c80cb8ed6b4b
DanFlame <danyelflame@gmail.com>
2023-02-05 15:08:20 +0500

    created temp directory for archive

--- zip_packaging.py
+++ zip_packaging.py
@@ -5,12 +5,18 @@ import zipfile
 
 def all_into_one():
     current_dir = path.dirname(path.abspath(__file__))
+    print('current dir = ', current_dir)
     resources_dir = path.join(current_dir, 'resources')
+    print('resources dir =', resources_dir)
+    tmp_dir = path.join(current_dir, 'tmp')
     files = os.listdir(resources_dir)
-    zip_path = path.join(resources_dir, 'my_archive.zip')
+    zip_path = path.join(tmp_dir, 'my_archive.zip')
 
     with zipfile.ZipFile(zip_path, mode='w', compression=zipfile.ZIP_DEFLATED) as my_archive:
         for file in files:
+            print(file)
             add_file = path.join(resources_dir, file)
             my_archive.write(add_file)
 
+
+all_into_one()

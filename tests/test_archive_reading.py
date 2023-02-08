b9df58aaa9bb56e440d434cef6fe338ddee37da1
DanFlame <danyelflame@gmail.com>
2023-02-08 23:10:03 +0500

    final changes(i wish)

--- tests/test_archive_reading.py
+++ tests/test_archive_reading.py
@@ -9,7 +9,6 @@ current_dir = path.dirname(path.abspath(__file__))
 project_dir = path.dirname(current_dir)
 sys.path.insert(0, project_dir)
 from zip_packaging import all_into_one
-from zip_packaging import extraction_file_path
 
 all_into_one()
 resources_dir = path.join(project_dir, 'resources')
@@ -19,7 +18,7 @@ zip_ = ZipFile(archive_path)
 
 
 def test_csv_check():
-    file_path = zip_.extract(extraction_file_path('my_csv.csv'), tmp_dir)
+    file_path = zip_.extract('my_csv.csv', tmp_dir)
 
     with open(file_path) as csv_file_path:
         csv_file = csv.reader(csv_file_path)
@@ -29,26 +28,26 @@ def test_csv_check():
 
 
 def test_xlsx_check():
-    file_path = zip_.extract(extraction_file_path('my_xlsx.xlsx'), tmp_dir)
+    file_path = zip_.extract('my_xlsx.xlsx', tmp_dir)
     book = xlrd.open_workbook(file_path)
     first_sheet = book.sheet_by_index(0)
     assert first_sheet.cell_value(rowx=4, colx=1) == 'Kathleen'
 
 
 def test_png_check():
-    file_path = zip_.extract(extraction_file_path('my_png.png'), tmp_dir)
+    file_path = zip_.extract('my_png.png', tmp_dir)
     assert path.getsize(file_path) == 23783
 
 
 def test_xls_check():
-    file_path = zip_.extract(extraction_file_path('my_xls.xls'), tmp_dir)
+    file_path = zip_.extract('my_xls.xls', tmp_dir)
     book = xlrd.open_workbook(file_path)
     first_sheet = book.sheet_by_index(0)
     assert first_sheet.cell_value(rowx=3, colx=2) == 'Gent'
 
 
 def test_pdf_check():
-    file_path = zip_.extract(extraction_file_path('my_pdf.pdf'), tmp_dir)
+    file_path = zip_.extract('my_pdf.pdf', tmp_dir)
     reader = PdfReader(file_path)
     first_page = reader.pages[0]
     text_from_first_page = first_page.extract_text()
--- zip_packaging.py
+++ zip_packaging.py
@@ -17,13 +17,4 @@ def all_into_one():
             print(file)
             add_file = path.join(resources_dir, file)
             print(add_file)
-            my_archive.write(add_file)
-
-
-def extraction_file_path(file_name):
-    current_dir = path.dirname(path.abspath(__file__))
-    resources_dir = path.join(current_dir, 'resources')
-    return path.join(resources_dir, file_name)[1:]
-
-
-all_into_one()
+            my_archive.write(add_file, path.basename(add_file))
